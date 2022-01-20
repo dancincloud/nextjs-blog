5ef8c1712fb945684c6fb411548505dfb0117f52
Joseph Li <joseph.li@vervemotion.com>
2022-01-20 11:53:39 -0500

    finish the first part of demo

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+/**
+ * @ Author: Joseph Li
+ * @ Create Time: 2022-01-19 12:47:57
+ * @ Modified time: 2022-01-19 15:19:37
+ * @ Description:
+ */
+
+import "../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
